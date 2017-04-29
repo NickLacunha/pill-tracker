038b1adfc3f993804ec00d35756e23f78fe2b2bd
NickLacunha <njpadinha@gmail.com>
2017-04-29 10:47:38 -0700

    added button to start inputmedication activity

--- PillTracker/app/src/main/java/adeel/pilltracker/MedicationSummary.java
+++ PillTracker/app/src/main/java/adeel/pilltracker/MedicationSummary.java
@@ -23,8 +23,7 @@ public class MedicationSummary extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                inputMedication(view);
             }
         });
     }
