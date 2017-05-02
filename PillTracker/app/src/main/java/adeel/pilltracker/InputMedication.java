51c4d6967a1313aff48a8daab9d9651804c54c81
NickLacunha <njpadinha@gmail.com>
2017-05-02 13:36:41 -0700

    Added code to read medications from database for summary page

--- PillTracker/app/src/main/java/adeel/pilltracker/InputMedication.java
+++ PillTracker/app/src/main/java/adeel/pilltracker/InputMedication.java
@@ -66,5 +66,6 @@ public class InputMedication extends AppCompatActivity {
         });
 
         AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
--- PillTracker/app/src/main/java/adeel/pilltracker/MedicationSummary.java
+++ PillTracker/app/src/main/java/adeel/pilltracker/MedicationSummary.java
@@ -1,6 +1,8 @@
 package adeel.pilltracker;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +11,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import adeel.pilltracker.db.PillTrackerContract;
+import adeel.pilltracker.db.PillTrackerDbHelper;
 
 public class MedicationSummary extends AppCompatActivity {
 
@@ -54,5 +59,34 @@ public class MedicationSummary extends AppCompatActivity {
     public void inputMedication(View view){
         Intent intent = new Intent(this, InputMedication.class);
         startActivity(intent);
+    }
+
+    public void readMedicationData(){
+        PillTrackerDbHelper dbHelper = new PillTrackerDbHelper(this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        // for a medication summary screen we only need to have the medication name
+        String[] projection = {
+                PillTrackerContract.Medication.COLUMN_NAME_MEDICATION_NAME
+        };
+
+        // we don't need any filters because we want the whole list of medications
+        String selection = "";
+        String[] selectionArgs = {};
+
+        // we also don't care about the sort order right now
+        String sortOrder = "";
+
+        Cursor cursor = db.query(
+                PillTrackerContract.Medication.TABLE_NAME,                     // The table to query
+                projection,                               // The columns to return
+                selection,                                // The columns for the WHERE clause
+                selectionArgs,                            // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
     }
 }
